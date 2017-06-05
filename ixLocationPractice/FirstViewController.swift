07b755d8735cfdda96a6d5c51930082d02b37d59
Claire Davis <claire.a.davis@vanderbilt.edu>
2017-06-05 09:39:50 +0200

    Initial Commit

---
+++ ixLocationPractice/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  ixLocationPractice
+//
+//  Created by Claire Davis on 6/5/17.
+//  Copyright © 2017 iXperience. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
