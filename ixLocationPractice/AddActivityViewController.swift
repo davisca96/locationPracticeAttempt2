9423906e59b7904186c3744c1723fa028a810a9b
Claire Davis <claire.a.davis@vanderbilt.edu>
2017-06-06 08:18:45 +0200

    Add Views

---
+++ ixLocationPractice/AddActivityViewController.swift
@@ -0,0 +1,41 @@
+//
+//  AddActivityViewController.swift
+//  ixLocationPractice
+//
+//  Created by Claire Davis on 6/5/17.
+//  Copyright © 2017 iXperience. All rights reserved.
+//
+
+import UIKit
+
+class AddActivityViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func save(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+
+    @IBAction func close(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- ixLocationPractice/FirstViewController.swift
+++ ixLocationPractice/FirstViewController.swift
@@ -11,21 +11,64 @@ import UIKit
 //needed for Apple Map Kit Usage
 import MapKit
 
+//need to add CLLocationManager to be able to inherent from/use manager class
+class FirstViewController: UIViewController, CLLocationManagerDelegate {
 
-class FirstViewController: UIViewController {
-
+    //declare location manager for use throughout app
+    //NEED ! after CLLocationManager
+    var locationManager: CLLocationManager!
+    
+    //need to declare to use location
+    var currentUserLocation: CLLocation!
+    
+    //declare map
     @IBOutlet weak var AppleMapView: MKMapView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        //instantiate location manager
+        locationManager = CLLocationManager()
+        
+        //assign delegate to self
+        locationManager.delegate = self
+        
+        //set accuracy level
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        
+        //request privacy access from user
+        locationManager.requestAlwaysAuthorization()
+      
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.startUpdatingLocation()
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        // Get the users location from the array of locations
+        let userLocation: CLLocation = locations[0] as CLLocation
+        
+        // You can call stopUpdatingLocation() to stop listening for location updates
+        // manager.stopUpdatingLocation()
+        
+        print("user latitude = \(userLocation.coordinate.latitude)")
+        print("user longitude = \(userLocation.coordinate.longitude)")
+        
+        // Store reference to the users location in the class instance (self)
+        self.currentUserLocation = userLocation
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error)
+    {
+        // An error occurred trying to retrieve users location
+        print("Error \(error)")
+    }
 }
 
